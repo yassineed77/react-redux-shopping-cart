b5f536a269f12846236420c8ad74664971cad6ee
Yassine Edahmani <yassineeda@gmail.com>
2021-09-14 17:39:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1>React Shopping Cart</h1>
+        <h1>Admin</h1>
+      </nav>
+
+      <body>
+        thee body
+      </body>
+
+      <footer>
+        <h3>footer</h3>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
